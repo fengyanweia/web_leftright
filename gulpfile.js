4173ffac18563e32700f740c5ee49913109e3b2b
fengyanwei <1217363491@qq.com>
2017-03-17 08:22:23 +0800

    asdasd

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+minifyHtml = require("gulp-minify-html"),
+cssmin = require('gulp-minify-css');
+gulp.task('myminifyhtml', function () {
+    gulp.src('*.html') // 要压缩的html文件
+    .pipe(minifyHtml()) //压缩
+    .pipe(gulp.dest('dist'));
+   
+});
+gulp.task('myminifycss', function () {
+    gulp.src('css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/css'));
+
+});
+gulp.task('default',['myminifyhtml','myminifycss']);
+
+
+
+
